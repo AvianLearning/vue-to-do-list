98bc4480dbc6e89f810cd36a72e6313990f6f23c
AvianLearning <iaradcliffe@hotmail.com>
2020-10-19 16:39:02 +0100

    Finished MVP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <h1>To-do List</h1>
+  <form v-on:submit.prevent="addNewItem">
+    <input id="new-item" type="text" v-model="newItem">
+    <!-- <input type="radio" name="priority" id="high" value="high" checked>
+    <label for="high">High</label>
+    <input type="radio" name="priority" id="low" value="low" checked>
+    <label for="low">Low</label> -->
+    <input type="submit" value="Add to-do">
+  </form>
+
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      <span>{{ item.name }}</span>
+    </li>
+
+
+  </ul>
+</div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [
+        {name: "Add PDA evidence", priority: "High"},
+        {name: "Cook the tea", priority: "High"},
+        {name: "De-scale kettle", priority: "Low"},
+        {name: "Invent time machine", priority: "High"},   
+      ],
+      newItem: ""
+    }
+  },
+  methods: {
+    addNewItem: function() {
+      this.items.push({
+        name: this.newItem,
+        priority: "Low"
+        });
+        this.newItem =""
   }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
